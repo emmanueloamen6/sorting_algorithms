55b1340bb962e2035809075df31c005275c1dd9a
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-04-27 03:41:46 -0700

    Sorting_algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+/**
+ * bubble_sort - sort array lements from min to max value
+ * @array: array
+ * @size: array size
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+	size_t i, j, tmp = 0;
+
+	if (size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1] && array[j + 1])
+			{
+			tmp = array[j];
+			array[j] = array[j + 1];
+			array[j + 1] = tmp;
+			print_array(array, size);
+			}
+		}
+}
