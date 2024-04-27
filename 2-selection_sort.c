55f8a73b92edac61d0c0a3587724015e3d1e0dc1
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-04-27 14:59:00 -0700

    Sorting Algorithms

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * selection_sort - sort the array by selection
+ * @array: array  to be sorted
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	int swap, flag = 0;
+	size_t min, i, j;
+
+	if (array == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		flag = 0;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+			{
+				min = j;
+				flag += 1;
+			}
+		}
+		swap = array[i];
+		array[min] = array[i];
+		array[min] = swap;
+		if (flag != 0)
+			print_array(array, size);
+	}
+}
