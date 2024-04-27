6ef0fdf761fbc7c35e00bf1981c6ff6364f83fba
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-04-27 03:46:21 -0700

    Sorting_algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,7 +9,7 @@ void bubble_sort(int *array, size_t size)
 
 	size_t i, j, tmp = 0;
 
-	if (size < 2)
+	if (array == NULL && size < 2)
 		return;
 	for (i = 0; i < size - 1; i++)
 		for (j = 0; j < size - 1; j++)
